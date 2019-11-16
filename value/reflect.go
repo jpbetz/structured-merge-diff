f9b399ae097e531f3ba1434d7b534232b7d9e0fb
Joe Betz <jpbetz@google.com>
2019-11-16 10:07:39 -0800

    clean up reflect code

--- value/reflect.go
+++ value/reflect.go
@@ -17,149 +17,152 @@ limitations under the License.
 package value
 
 import (
-	"fmt"
 	"reflect"
 )
 
-type ReflectValue struct {
+func Reflect(value interface{}) Value {
+	return reflectValue{Value: value}
+}
+
+type reflectValue struct {
 	Value interface{}
 }
 
-func (r ReflectValue) IsMap() bool {
+func (r reflectValue) IsMap() bool {
 	return isKind(r.Value, reflect.Map, reflect.Struct)
 }
 
-func (r ReflectValue) IsList() bool {
+func (r reflectValue) IsList() bool {
 	return isKind(r.Value, reflect.Slice, reflect.Array)
 }
-func (r ReflectValue) IsBool() bool {
+func (r reflectValue) IsBool() bool {
 	return isKind(r.Value, reflect.Bool)
 }
-func (r ReflectValue) IsInt() bool {
+func (r reflectValue) IsInt() bool {
 	// This feels wrong. Very wrong.
 	return isKind(r.Value, reflect.Int, reflect.Int64, reflect.Int32, reflect.Int16, reflect.Int8, reflect.Uint64, reflect.Uint, reflect.Uint32, reflect.Uint16, reflect.Uint8)
 }
-func (r ReflectValue) IsFloat() bool {
+func (r reflectValue) IsFloat() bool {
 	return isKind(r.Value, reflect.Float64, reflect.Float32)
 }
-func (r ReflectValue) IsString() bool {
+func (r reflectValue) IsString() bool {
 	return isKind(r.Value, reflect.String)
 }
-func (r ReflectValue) IsNull() bool {
+func (r reflectValue) IsNull() bool {
 	return reflect.ValueOf(r.Value).IsNil()
 }
-func (r ReflectValue) Map() Map {
-	if r.IsMap() {
-		return ReflectMap{r.Value}
+func (r reflectValue) Map() Map {
+	rval := deref(r.Value)
+	switch rval.Kind() {
+	case reflect.Struct:
+		return reflectStruct{r.Value}
+	case reflect.Map:
+		return reflectMap{r.Value}
+	default:
+		panic("value is not a map or struct")
 	}
-	panic("illegal cast")
 }
 
-func (r ReflectValue) List() List {
+func (r reflectValue) List() List {
 	if r.IsList() {
 		return ReflectList{r.Value}
 	}
-	panic("illegal cast")
+	panic("value is not a list")
 }
-func (r ReflectValue) Bool() bool {
+func (r reflectValue) Bool() bool {
 	if r.IsBool() {
 		return deref(r.Value).Bool()
 	}
-	panic("illegal cast")
+	panic("value is not a bool")
 }
-func (r ReflectValue) Int() int64 {
+func (r reflectValue) Int() int64 {
 	if r.IsInt() {
 		return deref(r.Value).Int()
 	}
-	panic("illegal cast")
+	panic("value is not an int")
 }
-func (r ReflectValue) Float() float64 {
+func (r reflectValue) Float() float64 {
 	if r.IsFloat() {
 		return deref(r.Value).Float()
 	}
-	panic("illegal cast")
+	panic("value is not a float")
 }
-func (r ReflectValue) String() string {
+func (r reflectValue) String() string {
 	if r.IsString() {
 		return deref(r.Value).String()
 	}
-	panic("illegal cast")
+	panic("value is not a string")
 }
-func (r ReflectValue) Interface() interface{} {
+func (r reflectValue) Interface() interface{} {
 	return r.Value
 }
 
-type ReflectMap struct {
+type reflectMap struct {
 	Value interface{}
 }
 
-func (r ReflectMap) Length() int {
+func (r reflectMap) Length() int {
 	rval := deref(r.Value)
-	switch rval.Type().Kind() {
-	case reflect.Struct:
-		return rval.NumField()
-	case reflect.Map:
-		return rval.Len()
-	default:
-		panic(fmt.Sprintf("unsupported kind: %v", reflect.TypeOf(r.Value).Kind()))
-	}
+	return rval.Len()
 }
 
-func (r ReflectMap) Get(key string) (Value, bool) {
+func (r reflectMap) Get(key string) (Value, bool) {
 	var val reflect.Value
 	rval := deref(r.Value)
-	switch rval.Type().Kind() {
-	case reflect.Struct:
-		val = rval.FieldByName(key)
-	case reflect.Map:
-		val = rval.MapIndex(reflect.ValueOf(key))
-	default:
-		panic(fmt.Sprintf("unsupported kind: %v", reflect.TypeOf(r.Value).Kind()))
-	}
-	zero := reflect.Value{}
-	return ReflectValue{val.Interface()}, val != zero
+	val = rval.MapIndex(reflect.ValueOf(key))
+	return reflectValue{val.Interface()}, val != zero
 }
 
-func (r ReflectMap) Set(key string, val Value) {
+func (r reflectMap) Set(key string, val Value) {
 	rval := deref(r.Value)
-	switch rval.Type().Kind() {
-	case reflect.Struct:
-		rval.FieldByName(key).Set(rval)
-	case reflect.Map:
-		rval.SetMapIndex(reflect.ValueOf(key), rval)
-	default:
-		panic(fmt.Sprintf("unsupported kind: %v", reflect.TypeOf(r.Value).Kind()))
-	}
+	rval.SetMapIndex(reflect.ValueOf(key), rval)
 }
 
-func (r ReflectMap) Delete(key string) {
+func (r reflectMap) Delete(key string) {
 	rval := deref(r.Value)
-	switch rval.Type().Kind() {
-	case reflect.Struct:
-		rval.FieldByName(key).Set(reflect.Value{})
-	case reflect.Map:
-		rval.SetMapIndex(reflect.ValueOf(key), reflect.Value{})
-	default:
-		panic(fmt.Sprintf("unsupported kind: %v", reflect.TypeOf(r.Value).Kind()))
-	}
+	rval.SetMapIndex(reflect.ValueOf(key), reflect.Value{})
 }
 
-func (r ReflectMap) Iterate(fn func(string, Value) bool) {
+func (r reflectMap) Iterate(fn func(string, Value) bool) {
 	rval := deref(r.Value)
-	switch rval.Type().Kind() {
-	case reflect.Struct:
-		for i := 0; i < rval.NumField(); i++ {
-			fn(rval.Type().Field(i).Name, ReflectValue{rval.Field(i).Interface()})
+	iter := rval.MapRange()
+	for iter.Next() {
+		if !fn(iter.Key().String(), reflectValue{iter.Value().Interface()}) {
+			return
 		}
-	case reflect.Map:
-		iter := rval.MapRange()
-		for iter.Next() {
-			if !fn(iter.Key().String(), ReflectValue{iter.Value().Interface()}) {
-				return
-			}
-		}
-	default:
-		panic(fmt.Sprintf("unsupported kind: %v", reflect.TypeOf(r.Value).Kind()))
+	}
+}
+
+type reflectStruct struct {
+	Value interface{}
+}
+
+func (r reflectStruct) Length() int {
+	rval := deref(r.Value)
+	return rval.NumField()
+}
+
+func (r reflectStruct) Get(key string) (Value, bool) {
+	var val reflect.Value
+	rval := deref(r.Value)
+	val = rval.FieldByName(key)
+	return reflectValue{val.Interface()}, val != zero
+}
+
+func (r reflectStruct) Set(key string, val Value) {
+	rval := deref(r.Value)
+	rval.FieldByName(key).Set(rval)
+}
+
+func (r reflectStruct) Delete(key string) {
+	rval := deref(r.Value)
+	rval.FieldByName(key).Set(reflect.Value{})
+}
+
+func (r reflectStruct) Iterate(fn func(string, Value) bool) {
+	rval := deref(r.Value)
+	for i := 0; i < rval.NumField(); i++ {
+		fn(rval.Type().Field(i).Name, reflectValue{rval.Field(i).Interface()})
 	}
 }
 
@@ -167,8 +170,13 @@ type ReflectList struct {
 	Value interface{}
 }
 
+// TODO: This function should not be part of the value.List interface
 func (r ReflectList) Interface() []interface{} {
-	return r.Value.([]interface{}) // TODO: This function should not be part of the interface
+	result := make([]interface{}, r.Length(), r.Length())
+	r.Iterate(func(i int, value Value) {
+		result[i] = value.Interface()
+	})
+	return result
 }
 
 func (r ReflectList) Length() int {
@@ -180,24 +188,20 @@ func (r ReflectList) Iterate(fn func(int, Value)) {
 	rval := deref(r.Value)
 	length := rval.Len()
 	for i := 0; i < length; i++ {
-		fn(i, ReflectValue{rval.Index(i).Interface()})
+		fn(i, reflectValue{rval.Index(i).Interface()})
 	}
 }
 
 func (r ReflectList) At(i int) Value {
 	rval := deref(r.Value)
-	return ReflectValue{rval.Index(i).Interface()}
+	return reflectValue{rval.Index(i).Interface()}
 }
+
+var zero = reflect.Value{}
 
 func isKind(val interface{}, kinds ...reflect.Kind) bool {
-	return reflectIsKind(reflect.ValueOf(val), kinds...)
-}
-
-func reflectIsKind(rval reflect.Value, kinds ...reflect.Kind) bool {
+	rval := deref(val)
 	kind := rval.Kind()
-	if kind == reflect.Ptr || kind == reflect.Interface {
-		return reflectIsKind(rval.Elem(), kinds...)
-	}
 	for _, k := range kinds {
 		if kind == k {
 			return true
